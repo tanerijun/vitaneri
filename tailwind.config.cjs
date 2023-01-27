dea409b6e53c8a6cbbc1d08dee7faf440fb605a4
tanerijun <tanerivince@gmail.com>
2023-01-27 22:52:41 +0800

    Fix tailwind config type issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkMode: 'class',
 	theme: {
 		extend: {}
 	},
-
 	plugins: []
 };
 
