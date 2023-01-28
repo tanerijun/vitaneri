cb8179526b39ce5dd2dd1a67e61e448cd830e63f
tanerijun <tanerivince@gmail.com>
2023-01-28 13:23:47 +0800

    Change theme implementation from using class to using data-theme

--- src/lib/components/ThemeToggler.svelte
+++ src/lib/components/ThemeToggler.svelte
@@ -9,10 +9,12 @@
 		if (darkTheme) {
 			document.documentElement.classList.remove('light');
 			document.documentElement.classList.add('dark');
+			document.documentElement.setAttribute('data-theme', 'dark');
 			localStorage.setItem('vitaneri:theme', 'dark');
 		} else {
 			document.documentElement.classList.remove('dark');
 			document.documentElement.classList.add('light');
+			document.documentElement.setAttribute('data-theme', 'light');
 			localStorage.setItem('vitaneri:theme', 'light');
 		}
 	}
