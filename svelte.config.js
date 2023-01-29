868ed3cd6310401187b99450049aaf3f097549de
tanerijun <tanerivince@gmail.com>
2023-01-29 10:28:59 +0800

    svelte-add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,21 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
 		preprocess({
 			postcss: true
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
