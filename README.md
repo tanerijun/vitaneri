8dbde5a1c27c0abf479bc06a61ba136e1824aec0
tanerijun <tanerivince@gmail.com>
2023-01-30 13:30:29 +0800

    Styling improvement

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## TODO
 
 - [ ] Style "Skip to content" in <header>
+- [ ] Make <header> responsive
+- [ ] Rewrite ThemeToggler with Headless Menu
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,7 +52,7 @@
 		<slot />
 	</main>
 
-	<footer class="flex justify-between">
+	<footer class="flex justify-between py-4">
 		<p>&#169; Copyright 2022-{new Date().getFullYear()} Vincent Taneri</p>
 		<div>Social Icons Placeholder</div>
 	</footer>
