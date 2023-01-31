7b3a2938a516136c39291088be6f2e12e474de55
tanerijun <tanerivince@gmail.com>
2023-01-31 11:17:09 +0800

    PostDate.svelte

---
+++ src/lib/components/PostDate.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { PostMetadata } from '$lib/types';
+	import { format, parseISO } from 'date-fns';
+
+	export let decorate = false;
+	export let post: PostMetadata;
+	export let collapsed = false;
+
+	let _class: string;
+	export { _class as class };
+</script>
+
+<div class={['relative z-10 order-first mb-3 flex', _class].join(' ')} class:pl-3.5={decorate}>
+	{#if decorate}
+		<span class="absolute inset-y-0 left-0 flex items-center py-1" aria-hidden="true">
+			<span class="h-full w-0.5 rounded-full bg-red-200" />
+		</span>
+	{/if}
+	<div class="flex" class:flex-col={!collapsed}>
+		<time datetime={post.datetime}>
+			{format(new Date(parseISO(post.datetime)), 'MMMM d, yyyy')}
+		</time>
+		{#if collapsed}
+			<span class="mx-1">&bull;</span>
+		{/if}
+		<span>{post.readingTime}</span>
+	</div>
+</div>
--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
 	import type { PostMetadata } from '$lib/types';
+	import PostDate from './PostDate.svelte';
 
 	export let posts: PostMetadata[];
 </script>
+
+<div>
+	{#each posts as post (post.slug)}
+		<article>
+			<PostDate class="hidden flex-col text-sm md:flex" {post} />
+		</article>
+	{/each}
+</div>
