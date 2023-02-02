727d28340686645e97d507bfdf15ab017ca93917
tanerijun <tanerivince@gmail.com>
2023-02-02 11:21:45 +0800

    Refactor Navbar

---
+++ src/lib/components/SiteHeader/Navbar/NavLinks.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { capitalizeFirstLetterOfWord } from '$lib/utils';
+
+	let routes = ['posts', 'projects', 'about'];
+</script>
+
+{#each routes as route (route)}
+	<li>
+		<a
+			class:text-text={$page.url.pathname.includes(route)}
+			class:active={$page.url.pathname.includes(route)}
+			class="relative hover:text-text"
+			href={`/${route}`}
+		>
+			{capitalizeFirstLetterOfWord(route)}
+		</a>
+	</li>
+{/each}
+
+<style lang="postcss">
+	/* .active::before { */
+	/* content: '';
+		position: absolute;
+		background: #fff;
+		height: 4px;
+		width: 10px;
+		bottom: -8px;
+		transition: 3s;
+		border-radius: 4px; */
+	/* content: '';
+		position: absolute;
+		width: 15px;
+		height: 15px;
+		-moz-border-radius: 7.5px;
+		-webkit-border-radius: 7.5px;
+		border-radius: 7.5px;
+		background-color: #69b6d5;
+	} */
+	.active {
+		@apply bg-red-400 text-text after:absolute after:h-6 after:w-6 after:animate-spin after:bg-foam after:content-[""];
+	}
+</style>
---
+++ src/lib/components/SiteHeader/Navbar/SiteLogo.svelte
@@ -0,0 +1,5 @@
+<div id="site-logo">
+	<a href="/">
+		<h1 class="select-none text-3xl tracking-wide">Vitaneri</h1>
+	</a>
+</div>
