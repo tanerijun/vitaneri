74328d721311c6f7a7df3c0236aef42e5b04cada
tanerijun <tanerivince@gmail.com>
2023-02-03 14:53:02 +0800

    Add social icons and refactor SearchIcon

--- src/lib/components/SiteHeader/Navbar/Search.svelte
+++ src/lib/components/SiteHeader/Navbar/Search.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
-	import Search from '$lib/components/icons/Search.svelte';
+	import SearchIcon from '$lib/components/icons/SearchIcon.svelte';
 </script>
 
 <Button>
-	<Search />
+	<SearchIcon />
 </Button>
