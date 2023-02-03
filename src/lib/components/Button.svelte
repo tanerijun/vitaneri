af1cb2a690e9c0d95aac6f53ba9890b0a71dc186
tanerijun <tanerivince@gmail.com>
2023-02-03 15:10:37 +0800

    Add social links

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	export let as: 'button' | 'a' = 'button';
+	export let href: string = '';
+	export let title: string = '';
+	$: isLink = as === 'a';
 	let _class: string = '';
 	export { _class as class };
 </script>
@@ -7,6 +10,9 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <svelte:element
 	this={as}
+	href={isLink && href ? href : null}
+	title={isLink && title ? title : null}
+	target={isLink ? '_blank' : null}
 	on:click
 	class={`flex items-center justify-center rounded-lg border border-muted bg-highlightLow p-2 transition-colors hover:border-subtle hover:text-text ${_class}`}
 >
--- src/lib/components/SiteFooter.svelte
+++ src/lib/components/SiteFooter.svelte
@@ -1,4 +1,31 @@
-<footer class="flex justify-between py-4">
+<script lang="ts">
+	import Button from './Button.svelte';
+	import EnvelopeIcon from './icons/EnvelopeIcon.svelte';
+	import GithubIcon from './icons/GithubIcon.svelte';
+	import LinkedInIcon from './icons/LinkedInIcon.svelte';
+	import TwitterIcon from './icons/TwitterIcon.svelte';
+</script>
+
+<footer class="flex items-center justify-between py-4">
 	<p>&#169; Copyright 2022-{new Date().getFullYear()} Vincent Taneri</p>
-	<div>Social Icons Placeholder</div>
+
+	<!-- Social links -->
+	<div class="flex items-center space-x-2">
+		<Button
+			as="a"
+			href="mailto:tanerivince@gmail.com"
+			title="Send an email to tanerivince@gmail.com"
+		>
+			<EnvelopeIcon />
+		</Button>
+		<Button as="a" href="https://github.com/tanerijun" title="See my Github profile">
+			<GithubIcon />
+		</Button>
+		<Button as="a" href="https://twitter.com/tanerivince" title="Say hi on Twitter">
+			<TwitterIcon />
+		</Button>
+		<Button as="a" href="https://www.linkedin.com/in/vincent-taneri/" title="Connect on LinkedIn">
+			<LinkedInIcon />
+		</Button>
+	</div>
 </footer>
