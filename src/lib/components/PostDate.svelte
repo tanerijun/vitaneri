b403e0e77c5e40136d5c762101e92536832115d3
tanerijun <tanerivince@gmail.com>
2023-02-03 22:39:34 +0800

    Styling improvement for PostDate

--- src/lib/components/PostDate.svelte
+++ src/lib/components/PostDate.svelte
@@ -13,7 +13,7 @@
 <div class={['relative z-10 order-first mb-3 flex', _class].join(' ')} class:pl-3.5={decorate}>
 	{#if decorate}
 		<span class="absolute inset-y-0 left-0 flex items-center py-1" aria-hidden="true">
-			<span class="h-full w-0.5 rounded-full bg-red-200" />
+			<span class="h-full w-1 rounded-full bg-muted" />
 		</span>
 	{/if}
 	<div class="flex" class:flex-col={!collapsed}>
