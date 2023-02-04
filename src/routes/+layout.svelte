9582e9ce964b8d43de0de79dcbc25201efbb3669
tanerijun <tanerivince@gmail.com>
2023-02-04 12:44:26 +0800

    Refactor LoadThemeScript

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
 	import '../app.postcss';
 	import Footer from './Footer.svelte';
 	import Header from './Header.svelte';
+	import LoadThemeScript from './LoadThemeScript.svelte';
 </script>
+
+<LoadThemeScript />
 
 <div id="site-container" class="container mx-auto px-6">
 	<Header />
---
+++ src/routes/LoadThemeScript.svelte
@@ -0,0 +1,12 @@
+<!-- @component
+  Script to run on page load to prevent screen flickering from theme change on first site visit
+-->
+<svelte:head>
+	<script>
+		let dataTheme = localStorage.getItem('vitaneri:theme');
+		if (!dataTheme) {
+			dataTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dusk' : 'dawn';
+		}
+		document.documentElement.setAttribute('data-theme', dataTheme);
+	</script>
+</svelte:head>
