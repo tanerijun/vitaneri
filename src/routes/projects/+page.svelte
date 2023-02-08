b4c9134cae488a99c482cf019556e408c6053707
tanerijun <tanerivince@gmail.com>
2023-02-08 13:11:48 +0800

    Add some contents for Projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -9,4 +9,24 @@
 	route={$page.url.pathname}
 />
 
-<p>TODO</p>
+<section>
+	<header>
+		<h1 class="text-4xl font-bold tracking-tight sm:text-5xl">Side Projects</h1>
+		<p class="mt-6">These are some of my side projects that I think are worth sharing:</p>
+	</header>
+
+	<footer>
+		<p>
+			You can check out more side projects on my
+			<a
+				href="https://github.com/tanerijun"
+				target="_blank"
+				rel="noreferrer"
+				class="hover-underline-animation text-iris"
+			>
+				Github
+			</a>
+			.
+		</p>
+	</footer>
+</section>
