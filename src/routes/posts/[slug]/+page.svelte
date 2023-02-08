324d9a66f447042c589e6675ed16b798e9fdb7b0
tanerijun <tanerivince@gmail.com>
2023-02-08 15:28:21 +0800

    Adjust style in posts/[slug] (WIP)

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -9,8 +9,6 @@
 
 	export let data: PageData;
 
-	const url = `https://vitaneri.com/posts/${data.post.slug}`;
-
 	// If we come to this route from the /posts route,
 	// use history when going back to preserve posts pagination
 	let canGoBack = false;
@@ -34,44 +32,42 @@
 />
 
 <!-- 42rem on large screen matches max-w-2xl on smaller screen-->
-<section class="mx-auto grid max-w-2xl grid-cols-1 lg:max-w-none lg:grid-cols-[1fr_42rem_1fr]">
-	<div class="hidden pt-8 lg:block">
-		<!-- goBack button -->
-		<div class="sticky top-0 flex w-full justify-end pt-11 pr-8">
-			<svelte:element
-				this={canGoBack ? 'button' : 'a'}
-				class="group -top-1 -left-16 mb-8 hidden h-10 w-10 items-center justify-center rounded-full border border-zinc-700/50 bg-white shadow-md shadow-zinc-800/5 ring-1 ring-white/10 transition hover:border-zinc-700 hover:ring-white/20 focus-visible:ring-2 lg:flex"
-				href={canGoBack ? undefined : '/posts'}
-				aria-label="Go back to posts"
-				on:click={goBack}
-				on:keydown={goBack}
-			>
-				<ArrowLeftIcon class="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700" />
-			</svelte:element>
+<!-- <section class="mx-auto grid max-w-2xl grid-cols-1 lg:max-w-none lg:grid-cols-[42rem_1fr]"> -->
+<section class="flex flex-col border border-blue-400">
+	<!-- <div class="mx-auto w-full overflow-x-hidden"> -->
+	<!-- goBack button -->
+	<div class="mb-8 w-fit font-heading text-iris">
+		<svelte:element
+			this={canGoBack ? 'button' : 'a'}
+			class="hover-underline-animation flex items-center gap-2"
+			href={canGoBack ? undefined : '/posts'}
+			aria-label="Go back to posts"
+			on:click={goBack}
+			on:keydown={goBack}
+		>
+			<ArrowLeftIcon class="h-4 w-4" /> Go Back
+		</svelte:element>
+	</div>
+
+	<article>
+		<!-- Post header -->
+		<header class="flex flex-col">
+			<h1 class="my-6 text-4xl font-bold tracking-tight sm:text-5xl">
+				{data.post.title}
+			</h1>
+			<PostDate class="text-sm" post={data.post} decorate collapsed />
+		</header>
+
+		<!-- Post content -->
+		<div class="prose mx-auto w-full border border-purple-400 dark:prose-invert">
+			<svelte:component this={data.component} />
 		</div>
-	</div>
-
-	<div class="mx-auto w-full overflow-x-hidden">
-		<article>
-			<!-- Post header -->
-			<header class="flex flex-col">
-				<h1 class="mt-6 text-4xl font-bold tracking-tight sm:text-5xl">
-					{data.post.title}
-				</h1>
-				<PostDate class="text-sm" post={data.post} decorate collapsed />
-			</header>
-
-			<!-- Post content -->
-			<div class="prose dark:prose-invert">
-				<svelte:component this={data.component} />
-			</div>
-		</article>
-	</div>
-
-	<!-- Table of contents -->
-	<div class="hidden pt-10 xl:block">
-		<aside class="sticky top-8 ml-8 hidden w-48 xl:block" aria-label="Table of Contents">
-			<TableOfContents post={data.post} />
-		</aside>
-	</div>
+	</article>
+	<!-- </div> -->
 </section>
+<!-- Table of contents -->
+<div class="hidden pt-10 xl:block">
+	<aside class="sticky top-8 ml-8 hidden w-48 xl:block" aria-label="Table of Contents">
+		<TableOfContents post={data.post} />
+	</aside>
+</div>
