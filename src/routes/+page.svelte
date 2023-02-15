6fcf2853ba6aa6cdd6856f49a5c79a7cf776b7ab
tanerijun <tanerivince@gmail.com>
2023-02-16 00:25:21 +0800

    Add an idea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,11 +11,18 @@
 <PageMeta
 	title="Vitaneri"
 	description="Personal website by Vincent Taneri"
-	route={$page.url.pathname}
-/>
+	route={$page.url.pathname} />
 
 <h1>Welcome to Vitaneri</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+	I am a software engineer based in XXX. I am currently working at XXX. This website is my humble
+	abode on the internet, where I pen my musings in here about software development, specifically
+	JavaScript, TypeScript and web technologies in general. If you enjoy my content, consider
+	subscribing to my RSS feed (link). If you feel so inclined, you're welcome to buy me a coffee
+	(link).
+</p>
 
 <section>Hero placeholder</section>
 
@@ -24,8 +31,7 @@
 		<h2 class="text-lg">Recently Published</h2>
 		<a
 			href="/posts"
-			class="hover-underline-animation flex items-center gap-1 font-heading text-sm text-iris"
-		>
+			class="hover-underline-animation flex items-center gap-1 font-heading text-sm text-iris">
 			View All <ArrowRightIcon class="h-4 w-4" />
 		</a>
 	</header>
