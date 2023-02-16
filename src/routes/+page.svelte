7014f99773f92571b6c8638089d4a16d696aa24a
tanerijun <tanerivince@gmail.com>
2023-02-16 23:47:35 +0800

    Refine index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,29 +11,47 @@
 <PageMeta
 	title="Vitaneri"
 	description="Personal website by Vincent Taneri"
-	route={$page.url.pathname} />
-
-<h1>Welcome to Vitaneri</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<p>
-	I am a software engineer based in XXX. I am currently working at XXX. This website is my humble
-	abode on the internet, where I pen my musings in here about software development, specifically
-	JavaScript, TypeScript and web technologies in general. If you enjoy my content, consider
-	subscribing to my RSS feed (link). If you feel so inclined, you're welcome to buy me a coffee
-	(link).
-</p>
-
-<section>Hero placeholder</section>
+	route={$page.url.pathname}
+/>
 
 <section>
-	<header class="mb-8 flex items-center justify-between">
-		<h2 class="text-lg">Recently Published</h2>
-		<a
-			href="/posts"
-			class="hover-underline-animation flex items-center gap-1 font-heading text-sm text-iris">
-			View All <ArrowRightIcon class="h-4 w-4" />
-		</a>
+	<header>
+		<h1 class="text-4xl font-bold tracking-tight sm:text-5xl">Welcome ~</h1>
+		<p class="mt-6">
+			This website is my humble abode on the internet, where I pen my musings about software
+			development, specifically JavaScript, TypeScript and web technologies in general. If you enjoy
+			my content, consider subscribing to my
+			<a
+				href="https://vitaneri.com/rss.xml"
+				target="_blank"
+				rel="noreferrer"
+				class="hover-underline-animation text-iris"
+			>
+				RSS feed
+			</a>
+			, and if you feel so inclined, you're welcome to
+			<a
+				href="https://vitaneri.com/rss.xml"
+				target="_blank"
+				rel="noreferrer"
+				class="hover-underline-animation text-iris"
+			>
+				buy me a coffee
+			</a>
+			.
+		</p>
 	</header>
-	<PostsList posts={data.posts} />
+
+	<section class="mt-16">
+		<header class="mb-8 flex items-center justify-between">
+			<h2 class="text-xl">Recently Published</h2>
+			<a
+				href="/posts"
+				class="hover-underline-animation flex items-center gap-1 font-heading text-sm text-iris"
+			>
+				View All <ArrowRightIcon class="h-4 w-4" />
+			</a>
+		</header>
+		<PostsList posts={data.posts} />
+	</section>
 </section>
