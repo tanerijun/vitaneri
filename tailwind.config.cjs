8dd95dc811b99488656accd037ad2e3af3152ed8
tanerijun <tanerivince@gmail.com>
2023-02-17 20:36:53 +0800

    Update Tailwind config to reflect changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,15 +12,9 @@ const config = {
 				muted: 'hsl(var(--color-muted) / <alpha-value>)',
 				subtle: 'hsl(var(--color-subtle) / <alpha-value>)',
 				text: 'hsl(var(--color-text) / <alpha-value>)',
-				love: 'hsl(var(--color-love) / <alpha-value>)',
-				gold: 'hsl(var(--color-gold) / <alpha-value>)',
-				rose: 'hsl(var(--color-rose) / <alpha-value>)',
-				pine: 'hsl(var(--color-pine) / <alpha-value>)',
-				foam: 'hsl(var(--color-foam) / <alpha-value>)',
 				iris: 'hsl(var(--color-iris) / <alpha-value>)',
 				highlightLow: 'hsl(var(--color-highlight-low) / <alpha-value>)',
-				highlightMed: 'hsl(var(--color-highlight-med) / <alpha-value>)',
-				highlightHigh: 'hsl(var(--color-highlight-high) / <alpha-value>)'
+				highlightMed: 'hsl(var(--color-highlight-med) / <alpha-value>)'
 			},
 			fontFamily: {
 				sans: ['Inter', ...fontFamily.sans],
