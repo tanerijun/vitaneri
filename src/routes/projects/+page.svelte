b2552429a9ef27aa5ee436649e92142ec2c21c2a
tanerijun <tanerivince@gmail.com>
2023-02-17 12:30:02 +0800

    Refine projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
 	import PageMeta from '$lib/components/PageMeta.svelte';
+	import { projects } from './projects';
 </script>
 
 <PageMeta
@@ -14,6 +15,42 @@
 		<h1 class="text-4xl font-bold tracking-tight sm:text-5xl">Side Projects</h1>
 		<p class="mt-6">These are some of my side projects that I think are worth sharing:</p>
 	</header>
+
+	<div class="columns-2">
+		{#each projects as project (project.title)}
+			<div class="mb-4 break-inside-avoid-column border border-muted/50">
+				<img src={project.imgUrl} alt={project.title} />
+				<div class="flex flex-col space-y-4 p-4">
+					<h2 class="text-2xl">{project.title}</h2>
+					<p>{project.description}</p>
+					<div class="columns-2 text-sm">
+						{#each project.techs as tech (tech)}
+							<div><span class="mr-2 text-iris">#</span>{tech}</div>
+						{/each}
+					</div>
+					<div>
+						<a
+							href={project.url}
+							class="hover-underline-animation text-iris"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Live
+						</a>
+						•
+						<a
+							href={project.repo}
+							class="hover-underline-animation text-iris"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Repo
+						</a>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
 
 	<footer>
 		<p>
---
+++ src/routes/projects/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+	{
+		title: 'Vitaneri',
+		description: 'The website you are currently on',
+		techs: ['TypeScript', 'Svelte', 'SvelteKit', 'TailwindCSS'],
+		imgUrl: '/assets/projects/vitaneri.png',
+		url: 'https://vitaneri.com',
+		repo: 'https://github.com/tanerijun/vitaneri'
+	},
+	{
+		title: 'Wordle Fever',
+		description: 'Wordle game for the true hardcore',
+		techs: ['TypeScript', 'React', 'ChakraUI', 'Framer Motion'],
+		imgUrl: '/assets/projects/wordle-fever.png',
+		url: 'https://wordle-fever.netlify.app/',
+		repo: 'https://github.com/tanerijun/wordle-fever'
+	},
+	{
+		title: 'Wowie',
+		description: 'A frontend for The Movie Database API',
+		techs: ['TypeScript', 'React', 'TailwindCSS', 'React Query'],
+		imgUrl: '/assets/projects/wowie.png',
+		url: 'https://wowie-fawn.vercel.app/',
+		repo: 'https://github.com/tanerijun/wowie'
+	},
+	{
+		title: 'React Speed Type',
+		description: 'A speed typing game created with React',
+		techs: ['TypeScript', 'React', 'TailwindCSS'],
+		imgUrl: '/assets/projects/speed-type.png',
+		url: 'https://speed-type-react.vercel.app/',
+		repo: 'https://github.com/tanerijun/speed-type-react'
+	}
+];
