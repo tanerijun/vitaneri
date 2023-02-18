89c0a397935f92ef88fc8b7fb978378250c0561f
tanerijun <tanerivince@gmail.com>
2023-02-18 20:15:11 +0800

    Replace adapter-auto in favor of adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -19,7 +19,13 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: true,
+			strict: true
+		}),
 		alias: {
 			$posts: './posts'
 		}
