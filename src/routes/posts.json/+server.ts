8f09d0b87266c4fa048e3335a44466e720fdb882
tanerijun <tanerivince@gmail.com>
2023-02-22 23:27:21 +0800

    Change data in posts.json

--- src/routes/posts.json/+server.ts
+++ src/routes/posts.json/+server.ts
@@ -4,5 +4,14 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 export const prerender = true;
 
 export const GET = (() => {
-	return json(posts.map((post) => post.title));
+	return json(
+		posts.map((post) => {
+			return {
+				title: post.title,
+				preview: post.preview,
+				slug: post.slug,
+				tags: post.tags
+			};
+		})
+	);
 }) satisfies RequestHandler;
