814d573df82601a1d0882d0cfec9380a12a24154
tanerijun <tanerivince@gmail.com>
2023-02-23 18:11:29 +0800

    Add Ctrl + K hint in Search and style improvements

--- src/routes/SearchLauncher.svelte
+++ src/routes/SearchLauncher.svelte
@@ -42,9 +42,17 @@
 		use:useModal
 		{...$modalAttrs}
 		transition:scale={{ duration: 150 }}
-		class="fixed left-1/2 top-1/2 z-50 flex h-[80%] w-[80%] -translate-x-1/2 -translate-y-1/2 flex-col space-y-4 rounded-xl bg-overlay/95 p-4 shadow-xl backdrop-blur-sm transition-all md:h-1/2 md:w-1/2 md:max-w-md md:px-4"
+		class="fixed left-1/2 top-1/2 z-50 flex h-[80%] w-[80%] -translate-x-1/2 -translate-y-1/2 flex-col space-y-4 rounded-xl bg-overlay/95 p-4 shadow-xl backdrop-blur-sm transition-all md:h-[70%] md:w-1/2 md:max-w-md md:px-4"
 	>
 		<Search on:navigation={closeModal} />
-		<p class="border-t border-muted/20 pt-4 text-xs">Test</p>
+		<p class="hidden border-t border-muted/20 pt-4 text-sm md:block">
+			Press <kbd>Ctrl</kbd> + <kbd>K</kbd> to search from anywhere.
+		</p>
 	</div>
 {/if}
+
+<style lang="postcss">
+	kbd {
+		@apply rounded-sm border border-muted py-0.5 px-1 text-xs;
+	}
+</style>
