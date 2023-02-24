317b8c75b6d2be124b30ab25bda7331432f4a2ef
tanerijun <tanerivince@gmail.com>
2023-02-24 17:06:51 +0800

    Add post generator

---
+++ content-generator/generatePost.js
@@ -0,0 +1,56 @@
+import * as fs from 'fs';
+import GithubSlugger from 'github-slugger';
+const slugger = new GithubSlugger();
+
+/**
+ * Generate a new post template.
+ *
+ * @param {string} path - The path to the folder where the post will be created.
+ * @param {string} title - The title of the post.
+ */
+export function generatePost(path, title, description = '', tags = '') {
+	const BASE_URL = path;
+	const postSlug = slugger.slug(title);
+	const processedTags = processTagsString(tags);
+
+	const frontmatter = {
+		datetime: new Date().toISOString(),
+		title,
+		slug: postSlug,
+		featured: 'false',
+		tags: processedTags,
+		description
+	};
+
+	fs.mkdirSync(`${BASE_URL}/${postSlug}/`);
+
+	const writeStream = fs.createWriteStream(`${BASE_URL}/${postSlug}/index.md`);
+
+	writeStream.write('---\n');
+	for (const [key, value] of Object.entries(frontmatter)) {
+		writeStream.write(`${key}: ${value}\n`);
+	}
+	writeStream.write('---\n');
+	writeStream.write('\n');
+	writeStream.write('{/* Happy writing! */}');
+	writeStream.write('\n');
+	writeStream.end();
+}
+
+/**
+ * Process the tags string into a YAML array.
+ *
+ * @param {string} tags - The tags string (ex: "tag1, tag2, tag3").
+ * @returns {string} The tags string in YAML array format.
+ */
+function processTagsString(tags) {
+	const separator = '\n  - ';
+	let res = '';
+
+	const tagsArray = tags.split(',').map((tag) => tag.trim());
+	tagsArray.forEach((tag) => {
+		res += separator + tag;
+	});
+
+	return res;
+}
