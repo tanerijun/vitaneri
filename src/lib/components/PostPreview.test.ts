2f39a4369ffb1ad7c62af89d77d17f955bd6c9d1
tanerijun <tanerivince@gmail.com>
2023-03-02 00:09:54 +0800

    Improve PostPreview test

--- src/lib/components/PostPreview.test.ts
+++ src/lib/components/PostPreview.test.ts
@@ -1,38 +1,68 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/svelte';
-
-import PostPreviewTest from './test-components/PostPreviewTest.svelte';
+import html from 'svelte-htm';
 import PostPreview from './PostPreview.svelte';
 import { mockPostData } from '$lib/test-utils/mockPostData';
 
+const post = {
+	...mockPostData,
+	preview: {
+		text: mockPostData.description,
+		html: `<p data-testid="html-preview">${mockPostData.description}</p>`
+	}
+};
+
+const basicPostPreview = html`
+<${PostPreview} post=${post}>
+	<slot slot="eyebrow">
+		<p>Hello from PostPreview's eyebrow</p>
+	</slot>
+</${PostPreview}>
+`;
+
 describe('PostPreview', () => {
 	it('should render post info properly', () => {
-		render(PostPreviewTest);
-
-		expect(screen.getByText(mockPostData.title)).toBeInTheDocument();
+		render(basicPostPreview);
+		expect(screen.getByText(post.title)).toBeInTheDocument();
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		expect(screen.getByText(mockPostData.preview.text!)).toBeInTheDocument();
+		expect(screen.getByText(post.preview.text!)).toBeInTheDocument();
 		expect(screen.getByText(/read/i)).toBeInTheDocument();
 	});
 
 	it('should render actions slot', () => {
-		render(PostPreviewTest);
+		render(basicPostPreview);
 		expect(document.querySelector(`div[slot="actions"]`)).toBeInTheDocument();
 	});
 
 	it('should render eyebrow slot', () => {
-		render(PostPreviewTest);
+		render(basicPostPreview);
 		expect(screen.getByText(/Hello from PostPreview's eyebrow/i)).toBeInTheDocument();
 	});
 
-	it('should render text wrapped in <p> tag when post.preview.html is not available (description is provided in frontmatter', () => {
-		render(PostPreview, { props: { post: mockPostData } });
+	it('should render text wrapped in <p> tag when post.preview.html is a plain text just like post.preview.text (description is provided in frontmatter', () => {
+		const post = {
+			...mockPostData,
+			preview: {
+				text: mockPostData.description,
+				html: mockPostData.description
+			}
+		};
+
+		const customPostPreview = html`
+		<${PostPreview} post=${post}>
+			<slot slot="eyebrow">
+				<p>Hello from PostPreview's eyebrow</p>
+			</slot>
+		</${PostPreview}>
+		`;
+
+		render(customPostPreview);
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		expect(screen.getByText(mockPostData.preview.text!).tagName).toBe('P');
 	});
 
 	it('should render html escaped text when post.preview.html is available (description is not provided in frontmatter)', () => {
-		render(PostPreviewTest);
+		render(basicPostPreview);
 		expect(screen.getByTestId('html-preview')).toBeInTheDocument();
 	});
 });
--- src/lib/components/test-components/README.md
+++
@@ -1,1 +0,0 @@
-The components in this folder are test specific components not to be used anywhere except for testing.
