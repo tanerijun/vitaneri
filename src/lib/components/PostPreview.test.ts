04c842a65a8f8a8573727a0e94fddfbba8961801
tanerijun <tanerivince@gmail.com>
2023-03-01 13:41:43 +0800

    Update PostPreview test

--- src/lib/components/PostPreview.test.ts
+++ src/lib/components/PostPreview.test.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/svelte';
 
 import PostPreviewTest from './test-components/PostPreviewTest.svelte';
+import PostPreview from './PostPreview.svelte';
 import { mockPostData } from '$lib/test-utils/mockPostData';
 
 describe('PostPreview', () => {
@@ -22,5 +23,16 @@ describe('PostPreview', () => {
 	it('should render eyebrow slot', () => {
 		render(PostPreviewTest);
 		expect(screen.getByText(/Hello from PostPreview's eyebrow/i)).toBeInTheDocument();
+	});
+
+	it('should render text wrapped in <p> tag when post.preview.html is not available (description is provided in frontmatter', () => {
+		render(PostPreview, { props: { post: mockPostData } });
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		expect(screen.getByText(mockPostData.preview.text!).tagName).toBe('P');
+	});
+
+	it('should render html escaped text when post.preview.html is available (description is not provided in frontmatter)', () => {
+		render(PostPreviewTest);
+		expect(screen.getByTestId('html-preview')).toBeInTheDocument();
 	});
 });
