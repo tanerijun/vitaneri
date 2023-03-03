048844a61da01d7b5110d687798337e9ab8c1849
tanerijun <tanerivince@gmail.com>
2023-03-03 13:26:57 +0800

    Update e2e test

--- tests/Search.spec.ts
+++ tests/Search.spec.ts
@@ -12,7 +12,7 @@ test.describe('Search', () => {
 
 	test('should be able to open and close search with keyboard shortcuts', async ({ page }) => {
 		await page.goto('http://localhost:5173');
-		await page.waitForTimeout(300);
+		await page.waitForTimeout(500);
 		await page.keyboard.press('Control+K');
 		await expect(page.getByPlaceholder('Type something...')).toBeVisible();
 		await page.keyboard.press('Control+K');
@@ -25,7 +25,7 @@ test.describe('Search', () => {
 
 	test('should be able to search for a post', async ({ page }) => {
 		await page.goto('http://localhost:5173');
-		await page.waitForTimeout(300);
+		await page.waitForTimeout(500);
 		await page.getByRole('button', { name: 'Search' }).click();
 		await page.getByPlaceholder('Type something...').fill('how to debug typescript in vscode');
 		await expect(
