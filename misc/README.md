c9bf690425312f81779a6321224efa003b0a247c
tanerijun <tanerivince@gmail.com>
2023-08-12 23:18:32 +0800

    Clean start

--- misc/README.md
+++
@@ -1,1 +0,0 @@
-Files in this folder don't get shipped to production. But they might be useful in the future.
--- src/routes/SiteLogo.svelte
+++
@@ -1,9 +0,0 @@
-<div id="site-logo">
-	<a href="/">
-		<span
-			class="select-none bg-gradient-to-r from-text to-accent bg-clip-text font-heading text-3xl font-bold text-transparent hover:animate-mesh-background"
-		>
-			Vitaneri
-		</span>
-	</a>
-</div>
