c57897f673abe6717f52510bcb26897cce5521a8
tanerijun <tanerivince@gmail.com>
2023-08-13 22:55:34 +0800

    Use sharp for image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
 	integrations: [tailwind({ applyBaseStyles: false })],
 	experimental: { assets: true },
+	image: {
+		service: sharpImageService(),
+	},
 });
