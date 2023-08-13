c3c533a9770e113a3f17fb43d302673842600101
tanerijun <tanerivince@gmail.com>
2023-08-13 22:22:19 +0800

    Render a post

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+import { z, defineCollection } from "astro:content";
+
+const postCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		datetime: z.date(),
+		title: z.string(),
+		tags: z.array(z.string()),
+		description: z.string(),
+	}),
+});
+
+export const collections = {
+	posts: postCollection,
+};
+
+// ---
+// datetime: 2022-10-21T15:06:20.401Z
+// title: "Type Unknown in TypeScript"
+// slug: type-unknown-in-typescript
+// featured: false
+// tags:
+// 	- typescript
+// description: "Exploring one of the primitive types in TypeScript: Unknown"
+// ---
