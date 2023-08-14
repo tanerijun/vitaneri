cb6872ee124baa65c4bbe6800c02c9cd10895ab3
tanerijun <tanerivince@gmail.com>
2023-08-14 15:39:42 +0800

    Mark active route

---
+++ src/utils/tw-join.ts
@@ -0,0 +1,11 @@
+export function twJoin(...classNames: Array<string | null | undefined | boolean>) {
+	let res = "";
+
+	for (const className of classNames) {
+		if (Boolean(className)) {
+			res += " " + className;
+		}
+	}
+
+	return res;
+}
