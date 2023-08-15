19d483c38f23c23200a09000914ff8b5b76f8fb9
tanerijun <tanerivince@gmail.com>
2023-08-15 15:59:46 +0800

    Add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,33 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
+
+export function remarkReadingTime() {
+	return function (tree, { data }) {
+		const textOnPage = toString(tree);
+		const readingTime = getReadingTime(textOnPage);
+		// readingTime.text will give us minutes read as a friendly string,
+		// i.e. "3 min read"
+		data.astro.frontmatter.minutesRead = readingTime.text;
+	};
+}
 
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx()],
-  experimental: {
-    assets: true
-  },
-  image: {
-    service: sharpImageService()
-  },
-  redirects: {
-    "/posts": "/"
-  }
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		mdx(),
+	],
+	experimental: {
+		assets: true,
+	},
+	image: {
+		service: sharpImageService(),
+	},
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+	},
 });
