910eb86e628266556e6c42cd9387e4395db57d6e
tanerijun <tanerivince@gmail.com>
2023-08-15 15:42:12 +0800

    Use nunito as font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+				sans: ["Nunito Sans Variable", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
