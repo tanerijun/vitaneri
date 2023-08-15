00ee7f8a5577d2f527d15a7189a7ff4a980435dd
tanerijun <tanerivince@gmail.com>
2023-08-15 17:49:43 +0800

    Font change and styling improvements

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection } from "astro:content";
 import { postSchema, projectSchema, tilSchema } from "../utils/schemas";
 
 const postCollection = defineCollection({
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["Nunito Sans Variable", ...defaultTheme.fontFamily.sans],
+				sans: ["Atkinson Hyperlegible", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
