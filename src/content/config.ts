f50166e43e11fc60d78be3c195c63f215217120a
tanerijun <tanerivince@gmail.com>
2023-08-16 13:19:28 +0800

    Refactor content collection config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,30 @@
-import { defineCollection } from "astro:content";
-import { postSchema, projectSchema, tilSchema } from "../utils/schemas";
+import { z, defineCollection } from "astro:content";
 
 const postCollection = defineCollection({
 	type: "content",
-	schema: postSchema,
+	schema: z.object({
+		datetime: z.date(),
+		title: z.string(),
+		tags: z.array(z.string()),
+	}),
 });
 
 const projectCollection = defineCollection({
 	type: "data",
-	schema: projectSchema,
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		techs: z.array(z.string()),
+		url: z.string().url().optional(),
+		repo: z.string().url().optional(),
+	}),
 });
 
 const tilCollection = defineCollection({
 	type: "content",
-	schema: tilSchema,
+	schema: z.object({
+		datetime: z.date(),
+	}),
 });
 
 export const collections = {
