cb082d890bfeb8324b73409d06c9aea21f21c13b
tanerijun <tanerivince@gmail.com>
2023-08-18 17:45:31 +0800

    Build SiteFooter

---
+++ src/data/my-info.ts
@@ -0,0 +1,11 @@
+export const myInfo: Record<string, string> = {
+	firstName: "Vincent",
+	lastName: "Taneri",
+	email: "tanerivince@gmail.com",
+	website: "https://vitaneri.com",
+	github: "https://github.com/tanerijun",
+	twitter: "https://twitter.com/tanerijun",
+	threads: "https://www.threads.net/@tanerijun",
+	blueSky: "https://bsky.app/profile/vitaneri.com",
+	linkedIn: "https://www.linkedin.com/in/vincent-taneri/",
+};
