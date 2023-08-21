368a5c32c8a0150fff240e15f3d7f20297dc0027
tanerijun <tanerivince@gmail.com>
2023-08-22 00:20:18 +0800

    Add icons to ThemeToggler

---
+++ src/components/icons/MoonIcon.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from "solid-js/jsx-runtime";
+
+export default function MoonIcon(props: JSX.IntrinsicElements["svg"]) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+			<path
+				fill="currentColor"
+				d="M10 7a7 7 0 0 0 12 4.9v.1c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2h.1A6.98 6.98 0 0 0 10 7Zm-6 5a8 8 0 0 0 15.062 3.762A9 9 0 0 1 8.238 4.938A7.999 7.999 0 0 0 4 12Z"
+			/>
+		</svg>
+	);
+}
