7b753648d7e9d855090d7fd41a9e9a4fa329d76c
tanerijun <tanerivince@gmail.com>
2023-08-23 22:08:55 +0800

    Mobile menu styling improvement

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -6,12 +6,12 @@ import { siteInfo } from "../data/site-info";
 
 export default function MobileMenu() {
 	return (
-		<Dialog.Root defaultOpen>
+		<Dialog.Root>
 			<Dialog.Trigger>
 				<MenuIcon class="h-6 w-6 text-zinc-600 transition-colors hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100" />
 			</Dialog.Trigger>
 			<Dialog.Portal>
-				<Dialog.Overlay class="animate-fade-out ui-expanded:animate-fade-in fixed inset-0 z-50 bg-white/50 backdrop-blur-sm dark:bg-black/50" />
+				<Dialog.Overlay class="animate-fade-out ui-expanded:animate-fade-in fixed inset-0 z-50 bg-white/60 backdrop-blur-sm dark:bg-black/60" />
 				<div class="fixed inset-0 z-50 flex items-center justify-center">
 					<Dialog.Content class="animate-scale-out ui-expanded:animate-scale-in z-50 h-full w-full px-16 py-24">
 						<div class="flex h-full flex-col items-center">
