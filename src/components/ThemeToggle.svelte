dce4f27d3dd7a4a0c2c26b71fdba01e0014149af
tanerijun <tanerivince@gmail.com>
2023-08-25 16:57:22 +0800

    Change ThemeToggle implementation

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -25,126 +25,14 @@
 	}
 </script>
 
-<button class="group rounded-full p-1 hover:bg-zinc-100 dark:hover:bg-zinc-800" on:click={toggleTheme}>
-	{#if theme === "light"}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			viewBox="0 0 24 24"
-			class="h-6 w-6 text-zinc-600 transition-colors group-hover:text-zinc-900 dark:text-zinc-400 group-hover:dark:text-zinc-100"
-		>
-			<g fill="none" stroke="currentColor" stroke-dasharray="2" stroke-dashoffset="2" stroke-linecap="round" stroke-width="2">
-				<path d="M0 0">
-					<animate
-						fill="freeze"
-						attributeName="d"
-						begin="0.6s"
-						dur="0.1s"
-						values="M12 19v1M19 12h1M12 5v-1M5 12h-1;M12 21v1M21 12h1M12 3v-1M3 12h-1"
-					/>
-					<animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.1s" values="2;0" />
-				</path>
-				<path d="M0 0">
-					<animate
-						fill="freeze"
-						attributeName="d"
-						begin="0.75s"
-						dur="0.1s"
-						values="M17 17l0.5 0.5M17 7l0.5 -0.5M7 7l-0.5 -0.5M7 17l-0.5 0.5;M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5"
-					/>
-					<animate fill="freeze" attributeName="stroke-dashoffset" begin="0.75s" dur="0.6s" values="2;0" />
-				</path>
-			</g>
-			<g fill="currentColor">
-				<path d="M15.22 6.03L17.75 4.09L14.56 4L13.5 1L12.44 4L9.25 4.09L11.78 6.03L10.87 9.09L13.5 7.28L16.13 9.09L15.22 6.03Z">
-					<animate fill="freeze" attributeName="fill-opacity" dur="0.2s" values="1;0" />
-				</path>
-				<path
-					d="M19.61 12.25L21.25 11L19.19 10.95L18.5 9L17.81 10.95L15.75 11L17.39 12.25L16.8 14.23L18.5 13.06L20.2 14.23L19.61 12.25Z"
-				>
-					<animate fill="freeze" attributeName="fill-opacity" begin="0.1s" dur="0.2s" values="1;0" />
-				</path>
-			</g>
-			<g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-				<path
-					d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
-				/>
-				<set attributeName="opacity" begin="0.3s" to="0" />
-			</g>
-			<mask id="lineMdMoonToSunnyOutlineTransition0">
-				<circle cx="12" cy="12" r="12" fill="#fff" /><circle cx="12" cy="12" r="8">
-					<animate fill="freeze" attributeName="r" begin="0.3s" dur="0.2s" values="8;4" />
-				</circle>
-				<circle cx="18" cy="6" r="12" fill="#fff">
-					<animate fill="freeze" attributeName="cx" begin="0.3s" dur="0.2s" values="18;22" />
-					<animate fill="freeze" attributeName="cy" begin="0.3s" dur="0.2s" values="6;2" />
-					<animate fill="freeze" attributeName="r" begin="0.3s" dur="0.2s" values="12;3" />
-				</circle>
-				<circle cx="18" cy="6" r="10">
-					<animate fill="freeze" attributeName="cx" begin="0.3s" dur="0.2s" values="18;22" />
-					<animate fill="freeze" attributeName="cy" begin="0.3s" dur="0.2s" values="6;2" />
-					<animate fill="freeze" attributeName="r" begin="0.3s" dur="0.2s" values="10;1" />
-				</circle>
-			</mask>
-			<circle cx="12" cy="12" r="10" fill="currentColor" mask="url(#lineMdMoonToSunnyOutlineTransition0)" opacity="0">
-				<set attributeName="opacity" begin="0.3s" to="1" />
-				<animate fill="freeze" attributeName="r" begin="0.3s" dur="0.2s" values="10;6" />
-			</circle>
-		</svg>
-	{:else}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			viewBox="0 0 24 24"
-			class="h-6 w-6 text-zinc-600 transition-colors group-hover:text-zinc-900 dark:text-zinc-400 group-hover:dark:text-zinc-100"
-		>
-			<g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-				<g stroke-dasharray="2">
-					<path d="M12 21v1M21 12h1M12 3v-1M3 12h-1">
-						<animate fill="freeze" attributeName="stroke-dashoffset" dur="0.1s" values="4;2" />
-					</path>
-					<path d="M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5">
-						<animate fill="freeze" attributeName="stroke-dashoffset" begin="0.1s" dur="0.1s" values="4;2" />
-					</path>
-				</g>
-				<path
-					d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
-					opacity="0"
-				>
-					<set attributeName="opacity" begin="0.25s" to="1" />
-				</path>
-			</g>
-			<g fill="currentColor" fill-opacity="0">
-				<path d="M15.22 6.03L17.75 4.09L14.56 4L13.5 1L12.44 4L9.25 4.09L11.78 6.03L10.87 9.09L13.5 7.28L16.13 9.09L15.22 6.03Z">
-					<animate fill="freeze" attributeName="fill-opacity" begin="0.3s" dur="0.2s" values="0;1" />
-				</path>
-				<path
-					d="M19.61 12.25L21.25 11L19.19 10.95L18.5 9L17.81 10.95L15.75 11L17.39 12.25L16.8 14.23L18.5 13.06L20.2 14.23L19.61 12.25Z"
-				>
-					<animate fill="freeze" attributeName="fill-opacity" begin="0.5s" dur="0.2s" values="0;1" />
-				</path>
-			</g>
-			<mask id="lineMdSunnyOutlineToMoonTransition0">
-				<circle cx="12" cy="12" r="12" fill="#fff" /><circle cx="12" cy="12" r="4">
-					<animate fill="freeze" attributeName="r" begin="0.05s" dur="0.2s" values="4;8" />
-				</circle>
-				<circle cx="22" cy="2" r="3" fill="#fff">
-					<animate fill="freeze" attributeName="cx" begin="0.05s" dur="0.2s" values="22;18" />
-					<animate fill="freeze" attributeName="cy" begin="0.05s" dur="0.2s" values="2;6" />
-					<animate fill="freeze" attributeName="r" begin="0.05s" dur="0.2s" values="3;12" />
-				</circle>
-				<circle cx="22" cy="2" r="1">
-					<animate fill="freeze" attributeName="cx" begin="0.05s" dur="0.2s" values="22;18" />
-					<animate fill="freeze" attributeName="cy" begin="0.05s" dur="0.2s" values="2;6" />
-					<animate fill="freeze" attributeName="r" begin="0.05s" dur="0.2s" values="1;10" />
-				</circle>
-			</mask>
-			<circle cx="12" cy="12" r="6" fill="currentColor" mask="url(#lineMdSunnyOutlineToMoonTransition0)">
-				<set attributeName="opacity" begin="0.25s" to="0" />
-				<animate fill="freeze" attributeName="r" begin="0.05s" dur="0.2s" values="6;10" />
-			</circle>
-		</svg>
-	{/if}
+<button
+	class="rounded-full p-1 text-zinc-600 transition-colors hover:bg-zinc-100 hover:text-zinc-900 dark:text-zinc-400 dark:hover:bg-zinc-800 dark:hover:text-zinc-100"
+	on:click={toggleTheme}
+>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512" class="h-6 w-6">
+		<path
+			fill="currentColor"
+			d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8S8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184c0 101.705-82.311 184-184 184z"
+		/>
+	</svg>
 </button>
