c14f4cef7ee201c6c68b7e5322f7c0b40358c6b5
tanerijun <tanerivince@gmail.com>
2024-01-22 22:38:29 +0800

    Serve pagefind resources for local development

--- src/lib/astro-integrations/pagefind.ts
+++ src/lib/astro-integrations/pagefind.ts
@@ -1,10 +1,30 @@
 import type { AstroIntegration } from "astro";
 import { execSync } from "child_process";
+import fs from "fs";
+import sirv from "sirv";
 
 export function pagefindIntegration(): AstroIntegration {
 	return {
 		name: "pagefind",
 		hooks: {
+			"astro:server:setup": ({ logger, server }) => {
+				if (!fs.existsSync("dist")) {
+					logger.error("Couldn't find Pagefind index.");
+					logger.warn("To enable local development. Make sure to build your project at least once.");
+					logger.warn("Skipping serving Pagefind index.");
+					return;
+				}
+
+				logger.info("Serving Pagefind index from previous build.");
+				const serve = sirv("dist", { dev: true, etag: true });
+				server.middlewares.use((req, res, next) => {
+					if (req.url?.startsWith("/pagefind")) {
+						serve(req, res, next);
+					} else {
+						next();
+					}
+				});
+			},
 			"astro:build:done": ({ logger }) => {
 				logger.info("Starting indexing process.");
 				const cmd = `npx pagefind --site "dist"`;
